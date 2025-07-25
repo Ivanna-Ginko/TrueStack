e69a08f8c641088e848954cf3d2afa648e8c1039
unknown <ivannabelichenko@gmail.com>
2025-07-25 23:13:56 +0300

    2

--- src/db/models/users.js
+++ src/db/models/users.js
@@ -22,6 +22,10 @@ const UserSchema = new Schema({
     type: Number,
     default: null,
   },
+  savedArticles: {
+  type: Number,
+  default: null,
+  },
 });
 
 UserSchema.methods.toJSON = function () {
