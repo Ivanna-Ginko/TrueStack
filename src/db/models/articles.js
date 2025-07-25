357445a524b0c7139f9a8881eeea43899e7e23f4
stillistica <rozhkovavika24@gmail.com>
2025-07-25 15:13:14 +0300

    add schema articles

--- src/db/models/articles.js
+++ src/db/models/articles.js
@@ -1,46 +1,41 @@
+// src/db/models/articles.js
 import { Schema, model } from 'mongoose';
 
+const articlesSchema = new Schema(
+  {
+    img: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    article: {
+      type: String,
+      required: true,
+    },
+    rate: {
+      type: Number,
+      default: 0,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      ref: 'Users',
+    },
+    date: {
+      type: Date,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
-const ArticleSchema = new Schema({
-  id: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  img: {
-    type: String,
-    default: ''
-  },
-   userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Users'
-  },
-  name: {
-    type: String,
-    required: true // автор
-  },
-
-  title: {
-    type: String,
-    required: true
-  },
-  desc: {
-    type: String,
-    default: ''
-  },
-  article: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    enum: ['popular', 'general'],
-    default: 'popular'
-  }
-}, {
-  timestamps: true,
-  versionKey: false,
-
-});
-
-export const ArticlesCollection = model('Article', ArticleSchema);
+export const ArticlesCollection = model('Articles', articlesSchema);
