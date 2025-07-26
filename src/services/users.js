3cfb7a30e00c443533ad88a69d2e8b3d7305d52f
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-07-27 02:25:24 +0300

    add services

--- src/services/users.js
+++ src/services/users.js
@@ -1,4 +1,6 @@
 import { UsersCollection } from '../db/models/user.js';
+import { ArticlesCollection } from '../db/models/articles.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
 export const getAllUsers = async () => {
   const users = await UsersCollection.find();
@@ -8,4 +10,40 @@ export const getAllUsers = async () => {
 export const getUserById = async (userId) => {
   const user = await UsersCollection.findById(userId);
   return user;
+};
+
+export const getCreatedArticlesOfUser = async ({
+  userId,
+  page = 1,
+  perPage = 12,
+  sortBy = '_id',
+  sortOrder = 'desc',
+  filter = {},
+}) => {
+  const skip = (page - 1) * perPage;
+
+  const baseFilter = { ownerId: userId };
+
+  if (filter.title) {
+    baseFilter.title = { $regex: new RegExp(filter.title, 'i') };
+  }
+  if (filter.category) {
+    baseFilter.category = filter.category;
+  }
+
+  const query = ArticlesCollection.find(baseFilter);
+
+  const [total, items] = await Promise.all([
+    ArticlesCollection.countDocuments(baseFilter),
+    query
+      .sort({ [sortBy]: sortOrder === 'asc' ? 1 : -1 })
+      .skip(skip)
+      .limit(perPage)
+      .lean(),
+  ]);
+
+  return {
+    items,
+    ...calculatePaginationData(total, perPage, page),
+  };
 };
