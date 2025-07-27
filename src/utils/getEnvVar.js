d17e8868b77228d9415bd43558d1891842844d34
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-07-27 04:18:12 +0300

    uncomment cloudinary& getEnvVars

--- src/utils/getEnvVar.js
+++ src/utils/getEnvVar.js
@@ -2,22 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-// export function getEnvVar(name, defaultValue) {
-//   const value = process.env[name];
+export function getEnvVar(name, defaultValue) {
+  const value = process.env[name];
 
-//   if (value) return value;
+  if (value) return value;
 
-//   if (defaultValue) return defaultValue;
+  if (defaultValue) return defaultValue;
 
-//   throw new Error(`Missing: process.env['${name}'].`);
-// }
-
-export function getEnvVar(name) {
-  if (!process.env[name]) {
-    console.warn(
-      `Warning: process.env['${name}'] is missing, returning empty string.`,
-    );
-    return '';
-  }
-  return process.env[name];
+  throw new Error(`Missing: process.env['${name}'].`);
 }
